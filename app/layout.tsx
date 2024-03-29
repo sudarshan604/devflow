fd0817f20d30c5a5ebf3acd9cc8cba31b46204b3
sudarshan604 <kshetri.sudarshann@gmail.com>
2024-03-29 07:14:35 +0545

    implement ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,28 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import "./global.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Devflow",
+  description: "A site to ask programming question.",
+  icons: {
+    icon: "../../favicon.ico",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "700", "800"],
+  variable: "--font-inter",
+});
+
+const SPaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export default function RootLayout({
   children,
@@ -7,10 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <ClerkProvider>
-        <body>{children}</body>
-      </ClerkProvider>
-    </html>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          f,
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.variable} ${SPaceGrotesk.variable}`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,41 @@
+import React, { useContext, useState, useEffect, createContext } from "react";
+
+interface ThemeContextTypes {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextTypes | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("");
+
+  const handleThemeChange = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.add("light");
+    }
+  };
+
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used with in Theme provider");
+  }
+
+  return context;
+}
