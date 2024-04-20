ec6c92e1a48ed1537633740aedfee729d4607052
sudarshan604 <kshetri.sudarshann@gmail.com>
2024-04-20 08:59:15 +0545

    created/model question tag and user

---
+++ database/user.model.ts
@@ -0,0 +1,32 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface IUser extends Document {
+  clerkID: string;
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  picture: string;
+  location?: string;
+  portfolioWebsite?: string;
+  reputation?: number;
+  saved: Schema.Types.ObjectId[];
+  joinedAt: Date;
+}
+
+const userSchema = new Schema({
+  clerkID: { type: String, required: true },
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  bio: { type: String },
+  picture: { type: String, required: true },
+  location: { type: String },
+  portfolioWebsite: { type: String },
+  reputation: { type: Number, default: 0 },
+  saved: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  joinedAt: { type: Date, default: Date.now },
+});
+const Question = models.Question || model("Question", userSchema);
+
+export default Question;
