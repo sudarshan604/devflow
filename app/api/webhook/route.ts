00547dc88c61d016451e06496fe8923d5779bda1
sudarshan kshetri <57833720+sudarshan604@users.noreply.github.com>
2024-05-01 06:41:09 +0545

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -59,12 +59,12 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
-    console.log("data=", evt.data);
+    console.log("user info=", evt.data);
 
     const mongoUser = await createUser({
       clerkId: id,
       name: `${first_name} ${last_name ? `${last_name}` : " "}`,
-      username: username!,
+      username: "sudarshan",
       email: email_addresses[0].email_address,
       picture: image_url,
     });
